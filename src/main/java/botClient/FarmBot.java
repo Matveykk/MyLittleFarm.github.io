34cb42bfe81a551d2e31a47a969210b65e93df5b
Matvey Boychencev <boymotya@gmail.com>
2025-02-17 19:10:30 +0300

    changes

--- src/main/java/botClient/FarmBot.java
+++ src/main/java/botClient/FarmBot.java
@@ -1,12 +1,19 @@
 package botClient;
 
 import DataBaseWork.WorkWithDB;
+import com.google.gson.Gson;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.webapp.WebAppData;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import resourcesUtil.PropertiesUtil;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.webapp.WebAppData;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import com.google.gson.Gson;
 
 import java.util.Random;
 
@@ -34,76 +41,100 @@ public class FarmBot extends TelegramLongPollingBot {
         }
 
         // Обработка данных из веб-приложения
-        if (update.getMessage().getWebAppData() != null) {
+        if (update.hasMessage() && update.getMessage().getWebAppData() != null) {
             WebAppData webAppData = update.getMessage().getWebAppData();
-            System.out.println(webAppData.getData() + " Data");
+            String data = webAppData.getData(); // Получаем данные
+            long chatId = update.getMessage().getChatId();
+
+            try {
+                // Парсим JSON
+                Gson gson = new Gson();
+                WebAppPayload payload = gson.fromJson(data, WebAppPayload.class);
+
+                // Обрабатываем данные
+                int carrots = payload.getCarrots();
+
+                // Отправляем ответ пользователю
+                System.out.println("Вы вырастили " + carrots + " морковок! Ваш ID: ");
+            } catch (Exception e) {
+                sendMessage(chatId, "Ошибка обработки данных: " + e.getMessage());
+            }
         }
     }
 
-    // Метод для отправки приветственного сообщения
-    private void sendMessage(long chatId) {
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
+        // Метод для отправки приветственного сообщения
+        private void sendMessage ( long chatId){
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
 
-        Random random = new Random();
-        String[] Answers = {"Hello! Let's plant\nIf you need some - /help\nOne more message - /start",
-                "Welcome to the Farm!\nIf you need some - /help\nOne more message - /start",
-                "Hello! Let's farm some wheat\nIf you need some - /help\nOne more message - /start"};
-        message.setText(Answers[random.nextInt(Answers.length)]);
+            Random random = new Random();
+            String[] Answers = {"Hello! Let's plant\nIf you need some - /help\nOne more message - /start",
+                    "Welcome to the Farm!\nIf you need some - /help\nOne more message - /start",
+                    "Hello! Let's farm some wheat\nIf you need some - /help\nOne more message - /start"};
+            message.setText(Answers[random.nextInt(Answers.length)]);
 
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Метод для отправки сообщения с помощью
+        private void sendHelpMessage ( long chatId){
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText("In process...");
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Метод для отправки сообщения по умолчанию (если команда не распознана)
+        private void sendDefaultMessage ( long chatId){
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText("Я не понимаю эту команду. Используйте \"Помощь\"");
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Метод для отправки сообщения с данными из веб-приложения
+        private void sendMessage ( long chatId, String text){
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText(text);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public String getBotUsername () {
+            return PropertiesUtil.get(BOT_USERNAME);
+        }
+
+        @Override
+        public String getBotToken () {
+            return PropertiesUtil.get(BOT_TOKEN);
+        }
+
+        private static class WebAppPayload {
+            private int carrots;
+            private long userId;
+
+            public int getCarrots() {
+                return carrots;
+            }
         }
     }
-
-    // Метод для отправки сообщения с помощью
-    private void sendHelpMessage(long chatId) {
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText("In process...");
-
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Метод для отправки сообщения по умолчанию (если команда не распознана)
-    private void sendDefaultMessage(long chatId) {
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText("Я не понимаю эту команду. Используйте \"Помощь\"");
-
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // Метод для отправки сообщения с данными из веб-приложения
-    private void sendMessage(long chatId, String text) {
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText(text);
-
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public String getBotUsername() {
-        return PropertiesUtil.get(BOT_USERNAME);
-    }
-
-    @Override
-    public String getBotToken() {
-        return PropertiesUtil.get(BOT_TOKEN);
-    }
-}
